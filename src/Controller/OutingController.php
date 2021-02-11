7670cf9724c6b345e34dd2e86339805abe53cc5c
LangkaWS <60548795+LangkaWS@users.noreply.github.com>
2021-02-11 17:55:30 +0100

    Add condition to outing cancel if user is organizer

--- src/Controller/OutingController.php
+++ src/Controller/OutingController.php
@@ -204,28 +204,37 @@ class OutingController extends AbstractController
     public function cancel(Request $request, Outing $outing): Response
     {
 
-        $stateRepo = $this->getDoctrine()->getRepository(State::class);
-        $form = $this->createForm(CancelOutingType::class, $outing);
-        $form->handleRequest($request);
+        if ($this->getUser()->getId() === $outing->getOrganizer()->getId()) {
+
+            $stateRepo = $this->getDoctrine()->getRepository(State::class);
+            $form = $this->createForm(CancelOutingType::class, $outing);
+            $form->handleRequest($request);
 
 
-        if ($form->isSubmitted() && $form->isValid()) {
-            $outing->setState($stateRepo->find(6));
-            $this->getDoctrine()->getManager()->flush();
+            if ($form->isSubmitted() && $form->isValid()) {
+                $outing->setState($stateRepo->find(6));
+                $this->getDoctrine()->getManager()->flush();
 
-            return $this->redirectToRoute('app_home');
+                return $this->redirectToRoute('app_home');
+            }
+
+            if($outing->getStartDate() <= (new DateTime())->sub(new DateInterval("P1M"))) {
+                $this->addFlash('warning', "Cette sortie est archivée, elle n'est plus consultable.");
+                return $this->redirectToRoute('app_home');
+            }
+
+            return $this->render('outing/cancel.html.twig', [
+                'outing' => $outing,
+                'form' => $form->createView(),
+                'action' => 'cancel'
+            ]);
+
+        } else {
+            $this->addFlash('warning', "Accès refusé : vous n'êtes pas l'organisateur de cette sortie.");
+            return $this->redirectToRoute('outing_show', [
+                    'id' => $outing->getId()
+                ]);
         }
-
-        if($outing->getStartDate() <= (new DateTime())->sub(new DateInterval("P1M"))) {
-            $this->addFlash('warning', "Cette sortie est archivée, elle n'est plus consultable.");
-            return $this->redirectToRoute('app_home');
-        }
-
-        return $this->render('outing/cancel.html.twig', [
-            'outing' => $outing,
-            'form' => $form->createView(),
-            'action' => 'cancel'
-        ]);
     }
 
     /**
